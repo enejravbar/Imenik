84a93f3b020bba74861aeb00c5a3e54924ec821e
Enej Ravbar <enej.ravbar@siol.net>
2016-07-07 15:08:12 +0200

    Dodatki k glavni strani, Zavihek skupine, Zavihek dodaj nov vnos

--- public/js/koda.js
+++ public/js/koda.js
@@ -4,22 +4,36 @@ $(document).ready(function(){
 
 function izberiZavihek(){
 
-	$(".kategorija").click(function() {
+		$(".kategorija").click(function() {
+					
+			$(".kategorija").css({"background-color" : "#333"});
+				
+			$(this).css({"background-color" : "#111"});
 
-		$(".kategorija").css({"background-color" : "#333"});
-		$(this).css({"background-color" : "#111"});
+			if($(this).attr("id")=="povezava-Domov"){
+				$(".menu").css({"display" : "none"});
+				$("#domov").css({"display" : ""});
+			}
+			if($(this).attr("id")=="povezava-DodajNovVpis"){
 
-		if($(this).attr("id")=="povezava-Domov"){
+				$(".menu").css({"display" : "none"});
+				$("#dodajNovVpis").css({"display" : ""});
+			}
+			if($(this).attr("id")=="povezava-Skupine"){
+				
+				$(".menu").css({"display" : "none"});
+				$("#skupine").css({"display" : ""});
+			}
+			
+				
+		});
+
+		$("#glava-naslov").click(function() {
+
+			$(".kategorija").css({"background-color" : "#333"});
+			$("#povezava-Domov").css({"background-color" : "#111"});
 			$(".menu").css({"display" : "none"});
 			$("#domov").css({"display" : ""});
-		}
-		if($(this).attr("id")=="povezava-DodajNovVpis"){
-			$(".menu").css({"display" : "none"});
-			$("#dodajNovVpis").css({"display" : ""});
-		}
-		if($(this).attr("id")=="povezava-Skupine"){
-			$(".menu").css({"display" : "none"});
-			$("#skupine").css({"display" : ""});
-		}
-	});
+		
+		});
 }
