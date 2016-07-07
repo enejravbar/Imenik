27c1c3dcc338384bba1d651359e20991f5b09900
Enej Ravbar <enej.ravbar@siol.net>
2016-07-07 12:52:27 +0200

    login stran, glavna stran

---
+++ public/js/koda.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	izberiZavihek();
+});
+
+function izberiZavihek(){
+
+	$(".kategorija").click(function() {
+
+		$(".kategorija").css({"background-color" : "#333"});
+		$(this).css({"background-color" : "#111"});
+
+		if($(this).attr("id")=="povezava-Domov"){
+			$(".menu").css({"display" : "none"});
+			$("#domov").css({"display" : ""});
+		}
+		if($(this).attr("id")=="povezava-DodajNovVpis"){
+			$(".menu").css({"display" : "none"});
+			$("#dodajNovVpis").css({"display" : ""});
+		}
+		if($(this).attr("id")=="povezava-Skupine"){
+			$(".menu").css({"display" : "none"});
+			$("#skupine").css({"display" : ""});
+		}
+	});
+}
