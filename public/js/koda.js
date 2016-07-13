633f909079868e16b29d10a91854a90497b6e701
Enej Ravbar <enej.ravbar@siol.net>
2016-07-13 09:16:02 +0200

    Povezava z bazo, dodajanje skupin in delovnih mest, začetek branja polj za vnos novega uporabnika

--- public/js/koda.js
+++ public/js/koda.js
@@ -1,8 +1,432 @@
+
+var timer,timer1;
+var tabelaSkupin=[];	
+var tabelaDelovnihMest=[];
+
 $(document).ready(function(){
 	izberiZavihek();
 	skrijIskalnik();
 	oznaciVse();
+	gumbDodajNovoSkupino();
+	pridobiVseSkupine();
+	gumbOsveziSkupine();
+
+	gumbDodajNovoDelovnoMesto();
+	pridobiVsaDelovnaMesta();
+	gumbOsveziDelovnaMesta();
+
+	// ----------- DODAJ NOV VPIS -------------
+	prikaziSkupine();
+	dodajanjeEmailov();
+	dodajanjeMobStevilk();
+	dodajanjeStacStevilk();
+	brisanjeVrsticPriRegistraciji();
+	prikaziDelovnaMesta();
+	registrirajNovegaZaposlenega();
+
 });
+
+//----------------------------------  DODAJANJE NOV VPIS -------------------------------
+
+
+
+var mobStevilka = $("#registracija-ime").val();
+var mobKratkaStevilka = $("#registracija-priimek").val();
+var stacStevilka = $("#registracija-ime").val();
+var stacKratkaStevilka = $("#registracija-priimek").val();
+
+function registrirajNovegaZaposlenega(){
+	$("#gumb-registriraj").click(function(){
+		var ime = $("#registracija-ime").val();
+		var priimek = $("#registracija-priimek").val();
+		var naslov = $("#registracija-naslov").val();
+		var delovnoMesto= $('#registracija-delovnoMesto').find(":selected").index();
+		var izbiraSkupine = pridobiIndekseIzbranih("registracija-izberiSkupine");	//tabela izbranih skupin
+
+		var tabelaEmailov =  pridobiEmaile("seznam-emailov");
+		var tabelaMobStevilk =  pridobiEmaile("seznam-emailov");
+		console.log("delovno mesto "+ delovnoMesto);
+		console.log("izbira skupine "+ izbiraSkupine);
+		console.log("tabela mailov "+ tabelaEmailov);
+
+	});
+		
+}
+
+function pridobiIndekseIzbranih(idSelecta){
+	var tabelaIndeksov=[];
+    $('#'+idSelecta+' option').each(function(i,v){
+        if (this.selected){
+        	tabelaIndeksov.push(i);
+        } 
+    });
+    return tabelaIndeksov;
+}
+
+function pridobiEmaile(idTabeleEmailov){
+	var tabelaEmailov=[];
+	var stevec=0;
+    $('#'+idTabeleEmailov+' td').each(function(){
+    	if(stevec%2==0){
+    		tabelaEmailov.push($(this).html());	
+    	}
+    	stevec++;
+    });
+    return tabelaEmailov;
+}
+
+function pridobiMobStevilke(idTabeleMobStevilk){
+	var tabelaMobStevilk=[];
+	var stevec=0;
+	
+	var objekt={
+		mobSt:"",
+		kratkaMobSt:""
+	}
+
+    $('#'+idTabeleMobStevilk+' td').each(function(){
+    	if(stevec==0){
+    		objekt.mobSt=$(this).html();
+    	}
+    	if(stevec==1){
+    		objekt.kratkaMobSt=$(this).html();
+    	}
+    	if(stevec==2){
+    		tabelaMobStevilk.push(objekt);	
+    		stevec==0;
+    	}
+    	stevec++;
+    });
+    return tabelaMobStevilk;
+}
+
+
+
+function prikaziSkupine(){
+	//console.log("Deluje test test test");
+	$("#registracija-izberiSkupine").html("");
+	tabelaSkupin = pridobiVseSkupine();
+	for(var i=0; i<tabelaSkupin.length; i++){
+		$("#registracija-izberiSkupine").append("<option>"+tabelaSkupin[i].ime_skupina+"</option>");
+	}
+	
+}
+
+function prikaziDelovnaMesta(){
+	//console.log("Deluje test test test");
+	$("#registracija-delovnoMesto").html("");
+	for(var i=0; i<tabelaDelovnihMest.length; i++){
+		$("#registracija-delovnoMesto").append("<option>"+tabelaDelovnihMest[i].ime_del_mesto+"</option>");
+	}
+	
+}
+
+function dodajanjeEmailov(){
+	$("#dodajEmail-gumb").click(function(){
+		var email=$("#dodajEmail").val();
+		var html="<tr style=\"border:0;\">" +
+                 "   <td>"+email+"</td>" +
+                 "   <td><span class=\"glyphicon glyphicon-trash registracija\" style=\"float:right;\"></span></td> </tr>";
+		$("#seznam-emailov").append(html);
+		
+	});
+	
+}
+
+function dodajanjeMobStevilk(){
+	$("#dodajMobStevilko-gumb").click(function(){
+		var mobStevilka=$("#dodajMobStevilko").val();
+		var kratkaMobStevilka=$("#dodajKratkoMobStevilko").val();
+
+		var html="<tr style=\"border:0;\">" +
+                 "   <td>"+mobStevilka+"</td>" +
+                 "   <td>"+kratkaMobStevilka+"</td>" +
+                 "   <td><span class=\"glyphicon glyphicon-trash registracija\" style=\"float:right;\"></span></td> </tr>";
+		$("#seznam-mobStevilk").append(html);
+	
+	});
+	
+}
+
+function dodajanjeStacStevilk(){
+	$("#dodajStacStevilko-gumb").click(function(){
+		var stacStevilka=$("#dodajStacStevilko").val();
+		var kratkaStacStevilka=$("#dodajKratkoStacStevilko").val();
+
+		var html="<tr style=\"border:0;\">" +
+                 "   <td>"+stacStevilka+"</td>" +
+                 "   <td>"+kratkaStacStevilka+"</td>" +
+                 "   <td><span class=\"glyphicon glyphicon-trash registracija\" style=\"float:right;\"></span></td> </tr>";
+		$("#seznam-stacStevilk").append(html);
+
+	});
+	
+}
+
+function brisanjeVrsticPriRegistraciji(){
+	$(document).on('click', '.registracija', function(){
+		console.log("zaznal sem!");
+		$(this).parent().parent().remove();
+
+	});	
+
+}
+
+function obvestiloNiEmailov(){
+	var stVrstic = $('#myTable tr').length;
+}
+//----------------------------------  DODAJANJE SKUPINE -------------------------------
+function gumbDodajNovoSkupino(){
+	$("#dodajSkupino-gumb").click(function(){
+
+		var imeNoveSkupine= $("#imeNoveSkupine").val();
+		dodajNovoSkupino(imeNoveSkupine);
+	});
+}
+
+function dodajNovoSkupino(imeNoveSkupine){
+
+	var zahtevek={
+		imeSkupine: imeNoveSkupine
+	}
+
+	$.ajax({
+		    type: "POST",
+		    url: "/dodajSkupino",
+		    dataType: 'json',
+		    contentType: 'application/json', 
+		    async: true,
+		    data: JSON.stringify(zahtevek),
+
+		    success: function (odgovor){
+	                odgovor=JSON.parse(odgovor);
+	                if(odgovor.vpisano){
+	                	clearTimeout(timer);
+						$("#dodajSkupino-okvir").css({display: ""});	
+			    		$("#dodajSkupino-okvir").css({
+	                    "display": "inline-block"
+		                });
+		                $("#dodajSkupino-okvir").attr({
+		                    "class": "alert alert-success fade-in"
+		                });
+		                $("#dodajSkupino").html("Skupina \""+imeNoveSkupine+"\" uspešno dodana.");
+		                pridobiVseSkupine();
+		                timer = setTimeout(function() {
+		                    $("#dodajSkupino-okvir").hide('slow');
+		                }, 4000);
+			    	}else{
+			    		$("#dodajSkupino-okvir").css({
+	                    "display": "inline-block"
+		                });
+		                $("#dodajSkupino-okvir").attr({
+		                    "class": "alert alert-danger fade-in"
+		                });
+		                $("#dodajSkupino").html(odgovor.sporocilo);
+		                timer = setTimeout(function() {
+		                    $("#dodajSkupino-okvir").hide('slow');
+		                }, 4000);
+		    	}
+	                
+		    }
+		});
+}
+
+function pridobiVseSkupine(){
+	
+	$.ajax({
+		    type: "POST",
+		    url: "/seznamSkupin",
+		    dataType: 'json',
+		    contentType: 'application/json', 
+		    async: false,
+		    data: JSON.stringify({}),
+
+		    success: function (odgovor){
+	                odgovor=JSON.parse(odgovor);
+	                //console.log("Tabela "+odgovor.podatki);
+	                //tabelaSkupin = odgovor.podatki;
+	                tabelaSkupin = obrniVrstniRedElementovVTabeli(odgovor.podatki);
+	                izpisiVseSkupine(tabelaSkupin);
+	                console.log("Korak1");
+
+		    }
+	});
+	console.log("Korak2");
+	return tabelaSkupin; 
+	
+}
+
+function obrniVrstniRedElementovVTabeli(tabela){
+	var temp;
+	for(var i=0; i<tabela.length; i++){
+		if(tabela.length-1-i == i){
+			break;
+		}
+		temp=tabela[i];
+		tabela[i]=tabela[tabela.length-1-i];
+		tabela[tabela.length-1-i]=temp;
+		if(tabela.length-1-2*i==1){
+			break;
+		}
+	}
+	return tabela;
+}
+
+function izpisiVseSkupine(tabelaSkupin){
+
+	$("#seznam-skupin").html("");
+	//console.log("V tabeli je " + tabela.length +" elementov.")
+	var html=""+
+				"<tr>"+
+				"	<td>V pod. bazi ni nobenih skupin.</td>"+
+				"	<td>"+
+				"		"+
+				"	</td>"+
+				"</tr>";
+
+	for(var i=0; i<tabelaSkupin.length-1; i++ ){
+		if(i==0){
+			html="";
+		}
+		    
+		html=html+"<tr>"+
+			"	<td>"+tabelaSkupin[i].ime_skupina+"</td>"+
+			"	<td>"+
+			"		<span class=\"glyphicon glyphicon-trash\" style=\"float:right;\"></span>"+
+			"	</td>"+
+			"</tr>";                             
+                                    
+                                 
+	}
+	$("#seznam-skupin").append(html);
+}
+
+function gumbOsveziSkupine(){
+	$("#gumb-osvezi").click(function(){
+		pridobiVseSkupine();
+	});
+}
+
+//----------------------------------  DODAJANJE DELOVNEGA MESTA -------------------------------
+
+function gumbDodajNovoDelovnoMesto(){
+	$("#dodajDelovnoMesto-gumb").click(function(){
+
+		var imeNovegaDelovnegaMesta= $("#imeNovegaDelovnegaMesta").val();
+
+		dodajNovoDelovnoMesto(imeNovegaDelovnegaMesta);
+	});
+}
+
+function dodajNovoDelovnoMesto(imeDelovnegaMesta){
+
+	var zahtevek={
+		imeNovegaDelovnegaMesta: imeDelovnegaMesta
+	}
+
+	$.ajax({
+		    type: "POST",
+		    url: "/dodajDelovnoMesto",
+		    dataType: 'json',
+		    contentType: 'application/json', 
+		    async: true,
+		    data: JSON.stringify(zahtevek),
+
+		    success: function (odgovor){
+	                odgovor=JSON.parse(odgovor);
+	                if(odgovor.vpisano){
+	                	clearTimeout(timer1);
+						$("#dodajDelovnoMesto-okvir").css({display: ""});
+			    		$("#dodajDelovnoMesto-okvir").css({
+	                    "display": "inline-block"
+		                });
+		                $("#dodajDelovnoMesto-okvir").attr({
+		                    "class": "alert alert-success fade-in"
+		                });
+		                $("#dodajDelovnoMesto").html("Delovno mesto \""+imeDelovnegaMesta+"\" uspešno dodano.");
+		                pridobiVsaDelovnaMesta();
+		                timer1 = setTimeout(function() {
+		                    $("#dodajDelovnoMesto-okvir").hide('slow');
+		                }, 4000);
+			    	}else{
+			    		$("#dodajDelovnoMesto-okvir").css({
+	                    "display": "inline-block"
+		                });
+		                $("#dodajDelovnoMesto-okvir").attr({
+		                    "class": "alert alert-danger fade-in"
+		                });
+		                $("#dodajDelovnoMesto").html(odgovor.sporocilo);
+		                timer1 = setTimeout(function() {
+		                    $("#dodajDelovnoMesto-okvir").hide('slow');
+		                }, 4000);
+		    	}
+	                
+		    }
+		});
+}
+
+function pridobiVsaDelovnaMesta(){
+
+	$.ajax({
+		    type: "POST",
+		    url: "/seznamDelovnihMest",
+		    dataType: 'json',
+		    contentType: 'application/json', 
+		    async: false,
+		    data: JSON.stringify({}),
+
+		    success: function (odgovor){
+	                odgovor=JSON.parse(odgovor);
+	                tabelaDelovnihMest=obrniVrstniRedElementovVTabeli(odgovor.podatki);
+	                izpisiVsaDelovnaMesta(tabelaDelovnihMest);
+
+		    }
+	});
+	
+}
+function gumbOsveziDelovnaMesta(){
+	$("#gumb-osvezi-del-mesta").click(function(){
+		pridobiVsaDelovnaMesta();
+	});
+}
+
+function izpisiVsaDelovnaMesta(tabelaDelovnihMest){
+
+	$("#seznamDelovnihMest").html("");
+	//console.log("V tabeli je " + tabela.length +" elementov.")
+	var html=""+
+				"<tr>"+
+				"	<td>V pod. bazi ni nobenih delovnih mest.</td>"+
+				"	<td>"+
+				"		"+
+				"	</td>"+
+				"</tr>";
+
+	for(var i=0; i<=tabelaDelovnihMest.length-1; i++ ){
+		if(i==0){
+			html="";
+		}
+		    
+		html=html+"<tr>"+
+			"	<td>"+tabelaDelovnihMest[i].ime_del_mesto+"</td>"+
+			"	<td>"+
+			"		<span class=\"glyphicon glyphicon-trash\" style=\"float:right;\"></span>"+
+			"	</td>"+
+			"</tr>";                             
+                                    
+                                 
+	}
+	$("#seznamDelovnihMest").append(html);
+}
+
+
+function izpisiTabelo(tabela){
+	for(var i=0; i<tabela.length; i++){
+		console.log(tabela[i]);
+	}
+}
+
+
 
 function izberiZavihek(){
 
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var util = require("util");
 var express = require('express');
 var expressSession = require('express-session');
 var path = require('path');
-var bodyParser= require('body-parser')
+var bodyParser = require('body-parser')
+var mysql = require('mysql');
 
 var streznik = express();
 
@@ -12,70 +13,241 @@ var http = require('http');
 
 var httpServer = http.createServer(streznik);
 
-httpServer.listen(80, function(){
-	console.log("Streznik posluša na vratih 80.");
+httpServer.listen(80, function() {
+    console.log("Streznik posluša na vratih 80.");
 });
 
-var adminPassword="pivkap";  // geslo administratorskega računa
+var adminPassword = "pivkap"; // geslo administratorskega računa
 
 streznik.use(express.static('public'));
 
 //  // Skrivni ključ za podpisovanje piškotkov
 streznik.use(bodyParser.json());
 streznik.use(
-  expressSession({
-    secret: '1234567890QWERTY',
-    saveUninitialized: true,    // Novo sejo shranimo
-    resave: false,              // Ne zahtevamo ponovnega shranjevanja
-    cookie: {
-      maxAge: 3600000           // Seja poteče po 60min neaktivnosti v ms
-    }
-  })
+    expressSession({
+        secret: '1234567890QWERTY',
+        saveUninitialized: true, // Novo sejo shranimo
+        resave: false, // Ne zahtevamo ponovnega shranjevanja
+        cookie: {
+            maxAge: 3600000 // Seja poteče po 60min neaktivnosti v ms
+        }
+    })
 );
 
-streznik.get("/", function(zahteva, odgovor){
+var pool = mysql.createPool({
+    host: '10.10.101.71',
+    user: 'imenik',
+    password: 'imenik',
+    database: 'imenik',
+    charset: 'UTF8_GENERAL_CI'
+});
 
-	if(!zahteva.session.uporabnik){
-		odgovor.sendFile(path.join(__dirname, 'public', 'stran-uporabnik.html'));
+/*connection.connect(function(napaka1){
+	if(!napaka1){
+		console.log("Povezava z podatkovno bazo je uspešno vzpostavljena.");
 	}else{
-		odgovor.sendFile(path.join(__dirname, 'public', 'stran-administrator.html'));
-		
+		console.log("NAPAKA! Povezava z podatkovno bazo NI VZPOSTAVLJENA.");
 	}
-	
+});*/
+
+streznik.get("/", function(zahteva, odgovor) {
+	zahteva.session.uporabnik="/";
+    if (!zahteva.session.uporabnik) {
+
+        odgovor.sendFile(path.join(__dirname, 'public', 'stran-uporabnik.html'));
+    } else {
+        odgovor.sendFile(path.join(__dirname, 'public', 'stran-administrator.html'));
+
+    }
+
 })
 
-streznik.get("/login", function(zahteva, odgovor){
-	odgovor.sendFile(path.join(__dirname, 'public', 'login.html'));
-})
-
-streznik.post("/odjava", function(zahteva, odgovor){
-	zahteva.session.uporabnik=null;
-	odgovor.json(JSON.stringify({odjava:true}));
-})
-
-streznik.post("/checkLogin", function(zahteva, odgovor){
-	
-	var uporabniskoIme = zahteva.body.username;
-	var geslo = zahteva.body.password;
-
-	console.log("username "+zahteva.body.username + "\npassword "+zahteva.body.password);
-	var ajaxOdgovor;
-	
-	if(uporabniskoIme=="admin" && geslo == adminPassword){
-		zahteva.session.uporabnik=uporabniskoIme;
-		ajaxOdgovor={
-			pravilno : true,
-			preusmeritev : "/"
-		}
-	}else{
-		ajaxOdgovor={
-			pravilno : false,
-			preusmeritev : "/"
-		}
-	}
-      	odgovor.json(JSON.stringify(ajaxOdgovor));
-})
-
-streznik.get("*", function(zahteva, odgovor){	
+streznik.get("/login", function(zahteva, odgovor) {
+	zahteva.session.uporabnik="/login";
+	if(zahteva.session.uporabnik){
 		odgovor.redirect("/");
+	}else{
+		odgovor.sendFile(path.join(__dirname, 'public', 'login.html'));
+	}
+    
 })
+
+streznik.post("/odjava", function(zahteva, odgovor) {
+    zahteva.session.uporabnik = null;
+    odgovor.json(JSON.stringify({
+        odjava: true
+    }));
+})
+
+
+streznik.post("/dodajSkupino", function(zahteva, odgovor) {
+
+    pool.getConnection(function(napaka1, connection) {
+
+        if (!napaka1) {
+            var imeNoveSkupine = zahteva.body.imeSkupine;
+
+            connection.query('INSERT INTO skupine (ime_skupina) VALUES (\"' + imeNoveSkupine + '\");', function(napaka2, info) {
+                if (!napaka2) {
+                    //console.log("Skupina uspešno vnešena! ID: "+ info.insertId);
+                    odgovor.json(JSON.stringify({
+                        vpisano: true,
+                        id: info.insertId
+
+                    }));
+                } else {
+                    //console.log("NAPAKA pri vnosu nove skupine: "+ info.insertId);
+                    odgovor.json(JSON.stringify({
+                        vpisano: false,
+                        id: null,
+                        sporocilo: "NAPAKA! Vnos ni bil uspešen."
+                    }));
+                }
+
+            });
+
+            connection.release();
+
+        } else {
+            odgovor.json(JSON.stringify({
+                vpisano: false,
+                id: null,
+                sporocilo: "NAPAKA! Ni povezave z pod. bazo."
+            }));
+        }
+    });
+});
+
+streznik.post("/seznamSkupin", function(zahteva, odgovor) {
+
+    pool.getConnection(function(napaka1, connection) {
+
+        if (!napaka1) {
+            var imeNoveSkupine = zahteva.body.imeSkupine;
+
+            connection.query('SELECT * FROM skupine;', function(napaka2, vrstice) {
+                if (!napaka2) {
+                    odgovor.json(JSON.stringify({
+                        uspeh: true,
+                        podatki: vrstice
+                    }));
+                    console.log(vrstice);
+                } else {
+                    odgovor.json(JSON.stringify({
+                        uspeh: false,
+                        podatki: null
+                    }));
+                }
+
+            });
+            connection.release();
+
+        } else {
+            odgovor.json(JSON.stringify({
+                vpisano: false,
+                id: null,
+                sporocilo: "NAPAKA! Ni povezave z pod. bazo."
+            }));
+        }
+    });
+
+});
+
+streznik.post("/dodajDelovnoMesto", function(zahteva, odgovor) {
+
+    pool.getConnection(function(napaka1, connection) {
+        if (!napaka1) {
+
+            var imeDelovnoMesto = zahteva.body.imeNovegaDelovnegaMesta;
+
+            connection.query('INSERT INTO delovno_mesto(ime_del_mesto) VALUES (\"' + imeDelovnoMesto + '\");', function(napaka2, info) {
+                if (!napaka2) {
+                    //console.log("Delovno mesto uspešno vnešeno! ID: "+ info.insertId);
+                    odgovor.json(JSON.stringify({
+                        vpisano: true,
+                        id: info.insertId
+                    }));
+                } else {
+                    console.log("NAPAKA pri vnosu novega delovnega mesta." + napaka2);
+                    odgovor.json(JSON.stringify({
+                        vpisano: false,
+                        id: null
+                    }));
+                }
+
+            });
+
+            connection.release();
+
+        } else {
+            odgovor.json(JSON.stringify({
+                vpisano: false,
+                id: null,
+                sporocilo: "NAPAKA! Ni povezave z pod. bazo."
+            }));
+        }
+    });
+});
+
+streznik.post("/seznamDelovnihMest", function(zahteva, odgovor) {
+
+    pool.getConnection(function(napaka1, connection) {
+        if (!napaka1) {
+            connection.query('SELECT * FROM delovno_mesto;', function(napaka2, vrstice) {
+                if (!napaka2) {
+                    odgovor.json(JSON.stringify({
+                        uspeh: true,
+                        podatki: vrstice
+                    }));
+                    console.log(vrstice);
+                } else {
+                    odgovor.json(JSON.stringify({
+                        uspeh: false,
+                        podatki: null
+                    }));
+                }
+
+            });
+            connection.release();
+
+        } else {
+            odgovor.json(JSON.stringify({
+                vpisano: false,
+                id: null,
+                sporocilo: "NAPAKA! Ni povezave z pod. bazo."
+            }));
+        }
+    });
+
+});
+
+
+
+
+streznik.post("/checkLogin", function(zahteva, odgovor) {
+
+    var uporabniskoIme = zahteva.body.username;
+    var geslo = zahteva.body.password;
+
+    //console.log("username "+zahteva.body.username + "\npassword "+zahteva.body.password);
+    var ajaxOdgovor;
+
+    if (uporabniskoIme == "admin" && geslo == adminPassword) {
+        zahteva.session.uporabnik = uporabniskoIme;
+        ajaxOdgovor = {
+            pravilno: true,
+            preusmeritev: "/"
+        }
+    } else {
+        ajaxOdgovor = {
+            pravilno: false,
+            preusmeritev: "/"
+        }
+    }
+    odgovor.json(JSON.stringify(ajaxOdgovor));
+})
+
+streznik.get("*", function(zahteva, odgovor) {
+    odgovor.redirect("/");
+})
+
